8edbc2efeae0af891664f926caf38c4ca2d7a053
nicoleprim <104589995+nicoleprim@users.noreply.github.com>
2022-05-23 17:31:11 -0300

    Exercicio 13

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -97,12 +97,24 @@ function retornaPessoaAnonimizada(pessoa) {
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+    pessoasAutorizadas = []
+    for (pessoa of pessoas) {
+        if (pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60) {
+            pessoasAutorizadas.push(pessoa)
+        }
+    }
+  return pessoasAutorizadas
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+    pessoasNãoAutorizadas = []
+    for (pessoa of pessoas) {
+        if (pessoa.altura < 1.5 || pessoa.idade <= 14 || pessoa.idade >= 60) {
+            pessoasNãoAutorizadas.push(pessoa)
+        }
+    }
+  return pessoasNãoAutorizadas
 }
 
 // EXERCÍCIO 14
