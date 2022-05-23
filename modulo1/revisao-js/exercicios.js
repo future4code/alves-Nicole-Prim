f310a8a197be148f6b49d2ae590d719f3c874ed2
nicoleprim <104589995+nicoleprim@users.noreply.github.com>
2022-05-23 19:02:31 -0300

    Exercicio 15A

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -128,10 +128,18 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+    consultasOrder = consultas.sort(function (a, b) {
+        if (a.nome > b.nome){
+        return 1
+        } 
+        if (a.nome < b.nome) {
+        return -1
+        }
+        return 0
+    })
+    return consultasOrder
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-   
 }
