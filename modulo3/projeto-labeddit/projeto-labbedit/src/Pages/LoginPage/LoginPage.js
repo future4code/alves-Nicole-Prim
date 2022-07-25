64aadb10097e1dec20141429393b10c9f993e948
Nicole Prim <nicolepprim@gmail.com>
2022-07-25 18:45:59 -0300

    pagina login ok

--- modulo3/projeto-labeddit/projeto-labbedit/src/Pages/LoginPage/LoginPage.js
+++ modulo3/projeto-labeddit/projeto-labbedit/src/Pages/LoginPage/LoginPage.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import Logo from '../../Assets/Logo.svg'
 import { DivImg, DivP, Form, InputUm, InputDois, ButtonForm, Inputs, ContainerHome, DivLine, ButtonSign } from './Styled'
-import {useForm} from '../../Hooks/useForm'
+import { useForm } from '../../Hooks/useForm'
 import Line from '../../Assets/Line.svg'
+import { goToSignUpPage, goToPostListPage } from '../../Routes/Coordinator'
+import { useNavigate } from 'react-router-dom'
+import { BASE_URL } from '../../Constants/urls'
+import axios from 'axios'
 
 const LoginPage = () => {
   const { form, onChange, cleanFields } = useForm({ email: "", password: "" })
 
-  const onSubmitForm = () => {
+  const navigate = useNavigate()
 
+  const onSubmitForm = (event) => {
+    event.preventDefault()
+    axios.post(`${BASE_URL}/users/login`, form)
+      .then((response) => {
+        localStorage.setItem('token', response.data.token)
+        goToPostListPage(navigate)
+        cleanFields()
+      }).catch((error) => {
+        alert("Usuário ou senha inválidos")
+        console.log(error.response)
+        cleanFields()
+      })
   }
 
   return (
@@ -21,32 +37,32 @@ const LoginPage = () => {
       </DivP>
       <Form onSubmit={onSubmitForm}>
         <Inputs>
-        <InputUm
-          name={"email"}
-          type={"email"}
-          onChange={onChange}
-          value={form.email}
-          placeholder={"E-mail"}
-          required
-        />
-        <InputDois
-          name={"password"}
-          type={"password"}
-          onChange={onChange}
-          value={form.password}
-          placeholder={"Senha"}
-          required
-        />
+          <InputUm
+            name="email"
+            type="email"
+            onChange={onChange}
+            value={form.email}
+            placeholder="E-mail"
+            required
+          />
+          <InputDois
+            name="password"
+            type="password"
+            onChange={onChange}
+            value={form.password}
+            placeholder="Senha"
+            required
+          />
         </Inputs>
         <ButtonForm>
-        <button type={"submit"}>Continuar</button>
+          <button type="submit">Continuar</button>
         </ButtonForm>
       </Form>
       <DivLine>
-      <img src={Line} alt="linha" />
+        <img src={Line} alt="linha" />
       </DivLine>
       <ButtonSign>
-      <button>Crie uma conta!</button>
+        <button onClick={() => goToSignUpPage(navigate)}>Crie uma conta!</button>
       </ButtonSign>
     </ContainerHome>
   )
