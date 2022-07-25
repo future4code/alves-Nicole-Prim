2bf8b3925317ff86c69b4b05fab9c5146fbeda58
Nicole Prim <nicolepprim@gmail.com>
2022-07-25 18:08:12 -0300

    estilização pagina login pronta

--- modulo3/projeto-labeddit/projeto-labbedit/src/Pages/LoginPage/LoginPage.js
+++ modulo3/projeto-labeddit/projeto-labbedit/src/Pages/LoginPage/LoginPage.js
@@ -1,17 +1,54 @@
 import React from 'react'
 import Logo from '../../Assets/Logo.svg'
-import {DivImg, DivP} from './Styled'
+import { DivImg, DivP, Form, InputUm, InputDois, ButtonForm, Inputs, ContainerHome, DivLine, ButtonSign } from './Styled'
+import {useForm} from '../../Hooks/useForm'
+import Line from '../../Assets/Line.svg'
 
 const LoginPage = () => {
+  const { form, onChange, cleanFields } = useForm({ email: "", password: "" })
+
+  const onSubmitForm = () => {
+
+  }
+
   return (
-    <>
+    <ContainerHome>
       <DivImg>
         <img src={Logo} alt="imagem logo" />
       </DivImg>
       <DivP>
-      <p>O projeto de rede social da Labenu</p>
+        <p>O projeto de rede social da Labenu</p>
       </DivP>
-    </>
+      <Form onSubmit={onSubmitForm}>
+        <Inputs>
+        <InputUm
+          name={"email"}
+          type={"email"}
+          onChange={onChange}
+          value={form.email}
+          placeholder={"E-mail"}
+          required
+        />
+        <InputDois
+          name={"password"}
+          type={"password"}
+          onChange={onChange}
+          value={form.password}
+          placeholder={"Senha"}
+          required
+        />
+        </Inputs>
+        <ButtonForm>
+        <button type={"submit"}>Continuar</button>
+        </ButtonForm>
+      </Form>
+      <DivLine>
+      <img src={Line} alt="linha" />
+      </DivLine>
+      <ButtonSign>
+      <button>Crie uma conta!</button>
+      </ButtonSign>
+    </ContainerHome>
   )
 }
 
--- modulo3/projeto-labeddit/projeto-labbedit/src/Pages/LoginPage/Styled.js
+++ modulo3/projeto-labeddit/projeto-labbedit/src/Pages/LoginPage/Styled.js
@@ -1,25 +1,34 @@
 import styled from 'styled-components'
 
+export const ContainerHome = styled.div`
+
+
+`
+
 export const DivImg = styled.div`
 @media(max-width: 480px) {
-margin-top: 8.31rem;
-margin-left: 8.37rem;
-position: absolute;
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: 6rem;
+position: relative;
+img{
 width: 9.5rem;
 height: 8.8rem;
+}
 }
 
 `
 export const DivP = styled.div`
 @media(max-width: 480px) {
-position: absolute;
-margin-left: 1.6rem;
-width: 23.62rem;
+display: flex;
+justify-content: center;
 height: 1.3rem;
-margin-top: 17.18rem;
 p{ 
 font-family: 'IBM Plex Sans';
+margin-top: 1rem;
 font-style: normal;
+position: absolute;
 font-weight: 300;
 font-size: 16px;
 line-height: 21px;
@@ -27,5 +36,104 @@ text-align: center;
 color: #000000
 }
 }
+`
 
+export const Form = styled.form`
+@media(max-width: 480px) {
+display: flex;
+flex-direction: column;
+justify-content: center;
+}
+`
+
+export const Inputs = styled.div`
+display: flex;
+justify-content: center;
+`
+
+export const InputUm = styled.input`
+@media(max-width: 480px) {
+box-sizing: border-box;
+width: 340px;
+height: 60px;
+position: absolute;
+background: #FFFFFF;
+border: 1px solid #D5D8DE;
+border-radius: 4px;
+top: 359px;
+}
+`
+export const InputDois = styled.input`
+@media(max-width: 480px) {
+box-sizing: border-box;
+width: 340px;
+height: 60px;
+position: absolute;
+background: #FFFFFF;
+border: 1px solid #D5D8DE;
+border-radius: 4px;
+top: 427px;
+}
+`
+
+export const ButtonForm = styled.div`
+@media(max-width: 480px) {
+display: flex;
+justify-content: center;
+padding: 13px, 133px, 13px, 133px;
+align-items: center;
+button{
+background: linear-gradient(90deg, #FF6489 0%, #F9B24E 100%);
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 18px;
+line-height: 25px;
+text-align: center;
+color: #FFFFFF;
+border: none;
+border-radius: 27px;
+position: absolute;
+width: 21rem;
+height: 51px;
+top: 543px;
+}
+}
+`
+
+export const DivLine = styled.div`
+@media(max-width: 480px) {
+display: flex;
+justify-content: center;
+position: relative;
+img{
+
+    width: 21rem;
+    margin-top: 360px;
+}
+}
+`
+
+export const ButtonSign = styled.div`
+@media(max-width: 480px) {
+padding: 13px, 133px, 13px, 133px;
+display: flex;
+justify-content: center;
+align-items: center;
+button{
+box-sizing: border-box;
+position: absolute;
+height: 51px;
+width: 21rem;
+top: 634px;
+border: 1px solid #FE7E02;
+border-radius: 27px;
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 18px;
+line-height: 25px;
+color: #FE7E02;
+}
+}
 `
