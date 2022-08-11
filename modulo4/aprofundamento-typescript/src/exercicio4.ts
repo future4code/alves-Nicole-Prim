22e490f0e4a3f859e0f3801dd228f778c56f3154
Nicole Prim <nicolepprim@gmail.com>
2022-08-11 17:21:21 -0300

    exercicio 11-08

---
+++ modulo4/aprofundamento-typescript/src/exercicio4.ts
@@ -0,0 +1,31 @@
+/* type pokemon = {
+	name: string,
+  types: string,
+	healthPoints: number
+}
+
+const pokemon1: pokemon = {
+  name: "Charmander",
+  types: "Fire",
+  healthPoints: 28
+}
+
+const pokemon2: pokemon = {
+  name: "Bulbasaur",
+  types: "Grass/Poison",
+  healthPoints: 31
+}
+
+const pokemon3: pokemon = {
+  name: "Squirtle",
+  types: "Water",
+  healthPoints: 35
+} */
+
+// b) eu rodaria o comando npm run start-4 porque no package.json já defini que com esse comando rodaria o tsc e o arquivo em js
+
+// c) no caso não
+
+// d) não conheço outra forma alem da apresentada na dica
+
+
---
+++ modulo4/aprofundamento-typescript/src/exercicio5.ts
@@ -0,0 +1,2 @@
+// acredito que a diferença seja a de que quando criamos colocamos apenas as confiurações necessárias.
+// Quando rodamos o comando o arquivo vem com todas as configurações padrão, mesmo desnecessárias.
