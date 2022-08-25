7d4b3def27ef904c70e61928cb97eeab219893ab
Nicole Prim <nicolepprim@gmail.com>
2022-08-25 16:35:01 -0300

    to-do-list

---
+++ modulo4/to-do-list/src/endpoints/createUser.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express'
+
+export default async function createUser(req: Request, res: Response){
+    try {
+        const { name, nickname, email } = req.body
+
+        if (!name || !nickname || !email) {
+            res.statusCode = 400
+            throw new Error("Por favor, informe todos os dados solicitados");
+        }
+
+        if (typeof name !== 'string' || typeof nickname !== 'string' || typeof email !== 'string') {
+            res.statusCode = 400
+            throw new Error("Algum dos parâmetros não é válido")
+        }
+
+        const newUser = {
+            id: Date.now() / 10000,
+            name,
+            nickname,
+            email
+        }
+
+       
+
+        res.status(201).send({ message: "Usuário cadastrado com sucesso" })
+
+    } catch (error: any) {
+        res.status(res.statusCode || 500).send({ messagem: error.message });
+    }
+}
+
