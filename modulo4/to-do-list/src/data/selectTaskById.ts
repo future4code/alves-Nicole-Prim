3c64b7313a52755e066d4995df15c69b8eddf0d9
Nicole Prim <nicolepprim@gmail.com>
2022-08-26 17:11:20 -0300

    cinco exercicios

--- modulo4/to-do-list/src/data/selectTaskById.ts
+++ modulo4/to-do-list/src/data/selectTaskById.ts
@@ -2,9 +2,9 @@ import connection from "../connection"
 
 export default async function selectTaskById(id: number){
     const result = await connection('TaskToDoList')
-    .where({id})
+    .where("TaskToDoList.id", id)
     .join('UserToDoList', 'TaskToDoList.creator_user_id', 'UserToDoList.id')
-    .select('TaskToDoList.title', 'TaskToDoList.description', 'TaskToDoList.status', 'TaskToDoList.limit_date', 'TaskToDoList.creator_user_id', 'UserToDoList.nickname')
+    .select('TaskToDoList.id', 'TaskToDoList.title', 'TaskToDoList.description', 'TaskToDoList.status', 'TaskToDoList.limit_date', 'TaskToDoList.creator_user_id', 'UserToDoList.nickname')
 
     return result
 }
--- modulo4/to-do-list/src/endpoints/getTaskById.ts
+++ modulo4/to-do-list/src/endpoints/getTaskById.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import selectTaskById from '../data/selectTaskById';
+import moment from 'moment'
 
 export default async function getTaskById(req: Request, res: Response){
     try {
@@ -18,12 +19,14 @@ export default async function getTaskById(req: Request, res: Response){
         }
 
         res.status(200).send({
+            id: findTask[0].id,
             title: findTask[0].title,
             description: findTask[0].description,
-            limit_date: findTask[0].split('-').reverse().join('/'),
+            limit_date: moment(findTask[0].limit_date, "YYY-MM-DD").format("DD/MM/YYYY"),
             status: findTask[0].status,
-            creator_user_id: findTask[0].creator_user_id})
-
+            creator_user_id: findTask[0].creator_user_id,
+            nickname: findTask[0].nickname
+        })
     } catch (error: any) {
         res.status(res.statusCode || 500).send({ messagem: error.message || error.sqlMessage });
     }
