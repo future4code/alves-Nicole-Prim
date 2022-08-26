314347bdc5b03c468de6a01643746784b959db61
Nicole Prim <nicolepprim@gmail.com>
2022-08-26 11:20:54 -0300

    segundo endpoint

--- modulo4/to-do-list/src/index.ts
+++ modulo4/to-do-list/src/index.ts
@@ -2,5 +2,8 @@ import { Request, Response } from 'express'
 import app from './app'
 import connection from './connection';
 import createUser from './endpoints/createUser';
+import getUserById from './endpoints/getUserById';
 
 app.post("/user", createUser);
+
+app.get("/user/:id", getUserById)
